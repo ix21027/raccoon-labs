b2873ea820cffbdad09d4a5ee0593d3d0d0068df
Ivan <khyzhniak.i@outlook.com>
2019-07-12 14:54:00 +0300

    lab 3

--- lab.js
+++ lab.js
@@ -41,9 +41,101 @@ class AbstractProduct {
         this.setName        = (new_name) => _name = new_name;
         this.setDescription = (desc)  => _description = desc;
         this.setPrice       = (price) => { if (parseInt(price) >= 0) _price = price }    
+            
+        this.getProductTileHTML = () => {
+            let li = document.createElement('li');
+            
+            let productOverlayDIV = document.createElement('div');
+            let productOverlayANCOR = document.createElement('a');
+            let productOverlayAncorIMG = document.createElement('img');
+            
+            let imgOverlayDIV = document.createElement('div');
+            let imgOverlayAnchor = document.createElement('a');
+            let imgOverlayAreaDIV = document.createElement('div');
+            let imgOverlayAreaH2 = document.createElement('h2');
+            let headingHolderDIV = document.createElement('div');
+            let priceSTRONG = document.createElement('strong');
+            let priceBoxDIV = document.createElement('div');
+            let regularPriceSPAN = document.createElement('span');
+            let priceSPAN = document.createElement('span');
+            let overlayTXTdiv = document.createElement('div');
+            let holderDIV = document.createElement('div');
+            let quickViewA = document.createElement('div');
+            
+            let productNameH3 = document.createElement('h3');
+            let productNameA = document.createElement('a');
+            
+            let priceDIV = document.createElement('div');
+            let regpriceSPAN = document.createElement('span');
+            let boxpriceSPAN = document.createElement('span');
+    
+            productOverlayDIV.className = "product-overlay";
+            productOverlayANCOR.href = '#';
+            productOverlayAncorIMG.src= "http://placehold.it/225/cccccc/0000&amp;text=I";
+            productOverlayAncorIMG.alt = _name;
+            productOverlayAncorIMG.height = 225;
+            productOverlayAncorIMG.width = 225;
+    
+            imgOverlayDIV.className = "img-overlay";
+            imgOverlayAnchor.href = "#";
+            imgOverlayAnchor.title = _name;
+            imgOverlayAreaDIV.className = "img-overlay-area";
+            imgOverlayAreaH2.className = "heading";
+            imgOverlayAreaH2.innerText = _name;
+            headingHolderDIV.className = "heading-holder";
+            priceSTRONG.className = "price";
+            priceBoxDIV.className = "price-box";
+            regularPriceSPAN.className = "regular-price";
+            priceSPAN.className = "price";
+            priceSPAN.innerHTML = "&euro;" + _price;
+            overlayTXTdiv.className = "overlay-txt";
+            holderDIV.className = "holder";
+            quickViewA.className = "quickview";
+            quickViewA.rel = "nofollow";
+            quickViewA.href = "#"; 
+            quickViewA.innerText = "Quickview";
+            
+            productNameH3.className = "product-name";
+            productNameA.href = "#";
+            
+            productNameA.innerText = _name;
+            priceDIV.className = "price-box";
+            regpriceSPAN.className = "regular-price";
+            boxpriceSPAN.className = "price";
+            boxpriceSPAN.innerHTML = "&euro;" + _price;
+    
+            li.appendChild(productOverlayDIV);
+            productOverlayDIV.appendChild(productOverlayANCOR);
+            productOverlayANCOR.appendChild(productOverlayAncorIMG);
+            
+            productOverlayDIV.appendChild(imgOverlayDIV);
+            imgOverlayDIV.appendChild(imgOverlayAnchor);
+            imgOverlayAnchor.appendChild(imgOverlayAreaDIV);
+            imgOverlayAreaDIV.appendChild(imgOverlayAreaH2)
+            
+            imgOverlayAreaDIV.appendChild(headingHolderDIV);
+            headingHolderDIV.appendChild(priceSTRONG);
+            priceSTRONG.appendChild(priceBoxDIV);
+            priceBoxDIV.appendChild(regularPriceSPAN);
+            regularPriceSPAN.appendChild(priceSPAN);
+    
+            imgOverlayDIV.appendChild(overlayTXTdiv);
+    
+            imgOverlayAreaDIV.appendChild(holderDIV);
+            holderDIV.appendChild(quickViewA);
+    
+            li.appendChild(productNameH3);
+            productNameH3.appendChild(productNameA);
+    
+            li.appendChild(priceDIV);
+            priceDIV.appendChild(regpriceSPAN);
+            regpriceSPAN.appendChild(boxpriceSPAN);
+    
+            return li;
+        }
     }
     getFullInformation() {
-        let wastedProps = ["getPriceForQuantity"];
+        let wastedProps = ["getPriceForQuantity", "getProductTileHTML"];
         let getters = Object.getOwnPropertyNames(this).filter(prop => prop.startsWith('get') && !(wastedProps.includes(prop))); 
         let info = getters.map( getter => `${getter.slice(3)} -> ${this[getter]()}`);
         return info.join("\n");
@@ -56,6 +148,7 @@ class AbstractProduct {
                 this[setter] = arg;
             }   
     }  
+    
 }
 
 let searchProducts = (products, query) => {
@@ -120,13 +213,13 @@ class Electronics extends AbstractProduct {
         this._power = data.power || 0;
     }
     
-    set warranty(newWarr) {
-        if (0 <= newWarr && newWarr <= 10) this._warranty = newWarr;
+    set warranty(newWarranty) {
+        if (0 <= newWarr && newWarr <= 10) this._warranty = newWarranty;
         else throw new Error('invalid warranty');
     }
 
-    set power(newPow) {
-        this._power = newPow;
+    set power(newPower) {
+        this._power = newPower;
     }
     
     get warranty() {
@@ -139,6 +232,20 @@ class Electronics extends AbstractProduct {
         return super.getFullInformation() + "\n"
             + "Power -> " + this.power + "\n"
             + "Warranty -> " + this.warranty
+    }
+}
+
+const check = {
+    mail(email) {
+        return /(^[A-Za-z\d]{1})([^@]{1,19})@([\w.!$%\&;’*+\/=?\^_-]{1,15})\.([A-Za-z]{1,5}$)/.test(email)
+    },
+  
+    phone(phone) {
+        return /^(\+[\d]{2})?(([\s-]*)(\()?([\s-]*)(\d)([\s-]*)(\d)([\s-]*)(\d)(\))?)(([\s-]*[\d][\s-]*){7})$/.test(phone)
+    },
+  
+    pass(pass) {
+        return /^(?=.*\d)(?=.*[!@#\$%\^&\*_-])(?=.*\w)[\d\w\s]{8,24}$/.test(pass) 
     }
 }
 
